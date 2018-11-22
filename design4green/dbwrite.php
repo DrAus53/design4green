08e96ece9acd1b9723285828a9af63b1140ec184
Damien Maymard <Damien Maymard@172.16.112.39>
2018-11-22 16:19:17 +0100

    0

--- design4green/dbwrite.php
+++ design4green/dbwrite.php
@@ -93,7 +93,7 @@ function registerSurveyResult() {
 
 <script>
 function sendSurveyResult() {
-    var msgFunc = "test"+<?php registerSurveyResult();?>
+    var msgFunc = "<?php registerSurveyResult();?>"
     document.getElementById("surveySaveLink").innerHTML = "Use this link to fill the survey later : http://vps612249.ovh.net "+msgFunc;
 }
 </script>
