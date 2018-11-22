51f92de0afca4f8c08871c5f056c0d0ce9fec810
Damien Maymard <Damien Maymard@172.16.112.39>
2018-11-22 19:30:10 +0100

    0

--- design4green/dbwrite.php
+++ design4green/dbwrite.php
@@ -38,94 +38,95 @@
 				<p id="buttonNextDescription" onclick="changeQuestion()">Let's do
 					this !</p>
 			</div>
+
+            <?php
+            echo '<br/>' . "\n";
+            echo '<center><strong>Brocos Team Survey</strong>';
+            echo '<br/>' . "\n";
+            echo '<br/>' . "\n";
+            ?>
+            
+            <button onclick="sendSurveyResult()">Save it</button>
+			<p id="surveySaveLink"></p>
+            	
+            <?php
+
+            echo '</center><br/>' . "\n";
+
+            $servername = "localhost";
+            $username = "damien";
+            $password = "brocos";
+            $dbname = "brocosurvey";
+
+            try {
+                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $stmtQuestion = $conn->prepare("SELECT id, type_reponse, valeur FROM Question");
+                $stmtQuestion->execute();
+                $state = "init";
+                $reponsePreced = "";
+                while ($donnesQuestion = $stmtQuestion->fetch()) {
+                    $idQ = $donnesQuestion['id'];
+                    $requete = "SELECT id, id_quest_suiv, valeur, champ_select, champ_texte FROM Reponse WHERE id_question=" . $idQ;
+                    $stmtReponse = $conn->prepare($requete);
+                    $stmtReponse->execute();
+                    echo '<br/>' . "\n";
+                    echo '<strong>' . $donnesQuestion['valeur'] . '</strong>';
+                    echo '<br/>' . "\n";
+                    echo '<br/>' . "\n";
+                    while ($donnesReponse = $stmtReponse->fetch()) {
+                        if ($donnesReponse['champ_select'] == null) {
+                            echo '&nbsp';
+                            echo '<input type="checkbox" name="test" value="value">';
+                            echo $donnesReponse['valeur'];
+                            echo '<br/>' . "\n";
+                        } else {
+                            echo '&nbsp';
+                            if ($reponsePreced != $donnesReponse['valeur']) {
+                                echo '&nbsp';
+                                echo $donnesReponse['valeur'];
+                                echo '<br/>' . "\n";
+                                echo '&nbsp';
+                                $reponsePreced = $donnesReponse['valeur'];
+                            }
+                            echo '<input type="checkbox" name="test" value="value">';
+                            echo '&nbsp';
+                            echo $donnesReponse['champ_select'];
+                            echo '<br/>' . "\n";
+                        }
+                    }
+                    echo '<br/>' . "\n";
+                }
+            } catch (PDOException $e) {
+                echo $state + "\n";
+                echo "Error: " . $e->getMessage();
+            }
+
+            function registerSurveyResult($conn)
+            {
+                $randomTitle = "save" . rand(1000, 9999);
+                try {
+                    $stmtInsert = $conn->prepare("INSERT INTO Questionnaire(titre) VALUES ('" . $randomTitle . "')");
+                    $stmtInsert->execute();
+                } catch (PDOException $e) {
+                    echo "Connection failed: " . $e->getMessage();
+                }
+                return $randomTitle;
+            }
+
+            $conn = null;
+
+            ?>
+
 		</form>
 	</div>
 
-<?php
-echo '<br/>' . "\n";
-echo '<center><strong>Brocos Team Survey</strong>';
-echo '<br/>' . "\n";
-echo '<br/>' . "\n";
-?>
-
-<button onclick="sendSurveyResult()">Save it</button>
-	<p id="surveySaveLink"></p>
-	
-<?php
-
-echo '</center><br/>' . "\n";
-
-$servername = "localhost";
-$username = "damien";
-$password = "brocos";
-$dbname = "brocosurvey";
-
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmtQuestion = $conn->prepare("SELECT id, type_reponse, valeur FROM Question");
-    $stmtQuestion->execute();
-    $state = "init";
-    $reponsePreced = "";
-    while ($donnesQuestion = $stmtQuestion->fetch()) {
-        $idQ = $donnesQuestion['id'];
-        $requete = "SELECT id, id_quest_suiv, valeur, champ_select, champ_texte FROM Reponse WHERE id_question=" . $idQ;
-        $stmtReponse = $conn->prepare($requete);
-        $stmtReponse->execute();
-        echo '<br/>' . "\n";
-        echo '<strong>' . $donnesQuestion['valeur'] . '</strong>';
-        echo '<br/>' . "\n";
-        echo '<br/>' . "\n";
-        while ($donnesReponse = $stmtReponse->fetch()) {
-            if ($donnesReponse['champ_select'] == null) {
-                echo '&nbsp';
-                echo '<input type="checkbox" name="test" value="value">';
-                echo $donnesReponse['valeur'];
-                echo '<br/>' . "\n";
-            } else {
-                echo '&nbsp';
-                if ($reponsePreced != $donnesReponse['valeur']) {
-                    echo '&nbsp';
-                    echo $donnesReponse['valeur'];
-                    echo '<br/>' . "\n";
-                    echo '&nbsp';
-                    $reponsePreced = $donnesReponse['valeur'];
-                }
-                echo '<input type="checkbox" name="test" value="value">';
-                echo '&nbsp';
-                echo $donnesReponse['champ_select'];
-                echo '<br/>' . "\n";
-            }
-        }
-        echo '<br/>' . "\n";
+	<script>
+    function sendSurveyResult() {
+        var msgFunc = "<?php echo registerSurveyResult($conn);?>"
+        document.getElementById("surveySaveLink").innerHTML = "Use this link to fill the survey later : http://vps612249.ovh.net/"+msgFunc+".php";
     }
-} catch (PDOException $e) {
-    echo $state + "\n";
-    echo "Error: " . $e->getMessage();
-}
-
-function registerSurveyResult($conn)
-{
-    $randomTitle = "save" . rand(1000, 9999);
-    try {
-        $stmtInsert = $conn->prepare("INSERT INTO Questionnaire(titre) VALUES ('" . $randomTitle . "')");
-        $stmtInsert->execute();
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-    }
-    return $randomTitle;
-}
-
-$conn = null;
-
-?>
-
-<script>
-function sendSurveyResult() {
-    var msgFunc = "<?php echo registerSurveyResult($conn);?>"
-    document.getElementById("surveySaveLink").innerHTML = "Use this link to fill the survey later : http://vps612249.ovh.net/"+msgFunc+".php";
-}
-</script>
+    </script>
 
 </body>
 </html>
