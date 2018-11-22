4eb4547e3c4afbb974ae4e3415b04d7fa8b1b2ad
EmmaMansalier <emma.mansalier@reseau.eseo.fr>
2018-11-22 18:30:22 +0100

    t

--- design4green/pageweb.php
+++ design4green/pageweb.php
@@ -61,8 +61,12 @@ function createSingleChoice(){
 		?>";
 
 
-		
-
+		for (i=1; i<<?php echo $tailleRep; ?>; i++) { 
+	 		var input = divRep.appendChild(document.createElement("input"));
+	 		input.type="radio";
+			input.innerHTML=<?php echo $f[i]; ?>
+			
+		}
 
 }
 
