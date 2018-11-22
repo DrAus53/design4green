bc65f8e4b1872801a593a70264ae189f5944408e
EmmaMansalier <emma.mansalier@reseau.eseo.fr>
2018-11-22 16:21:45 +0100

    #Je comprends pas ton message aves les signes ...

--- design4green/pageweb.php
+++ design4green/pageweb.php
@@ -24,7 +24,7 @@
 <p onclick="myFunction()"></p>
 
 <form>
- <div id="questionultiple">
+ <div id="questionmultiple">
  	 <fieldset>
  		 <div id="question">sucess</div>
     		  <p class="flip" onclick="myFunction(1)">Question suivante</p>
