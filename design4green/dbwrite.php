a21e574d0cf3c29458697cfc8e7df8fb0a708e4b
Damien Maymard <Damien Maymard@172.16.112.39>
2018-11-22 16:54:08 +0100

    0

--- design4green/dbwrite.php
+++ design4green/dbwrite.php
@@ -85,7 +85,20 @@ try {
 $conn = null;
 
 function registerSurveyResult() {
-    include ('dbregister.php');
+
+    $msgFunc = "fail";
+    
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //$stmtInsert = $conn->prepare("SELECT id, type_reponse, valeur FROM Question");
+        $msgFunc = "success";
+    }
+    catch(PDOException $e)
+    {
+        echo "Connection failed: " . $e->getMessage();
+    }
+    
     return $msgFunc;
 }
 
