f968e964d0b405eefda34adacb8b1ddf46dec2ef
Damien Maymard <Damien Maymard@DESKTOP-V7Q7G8J>
2018-11-22 22:52:50 +0100

    0

--- design4green/survey.php
+++ design4green/survey.php
@@ -49,15 +49,21 @@ include ('dbread.php');
         
     	var question = document.getElementById("surveyQuestion");
     	var answer = document.getElementById("surveyAnswer");
-		if (compteur==2) {
+		if (compteur==1) {
+			<?php $q=$questions[1];?>
+			answer.innerHTML="dans if compt 1";
+		} else if (compteur==2) {
 			<?php $q=$questions[2];?>
+			answer.innerHTML="dans if compt 2";
 		} else if (compteur==3) {
 			<?php $q=$questions[3];?>
+			answer.innerHTML="dans if compt 3";
 		} else {
-	    	<?php $q=$questions[1];?>
+	    	<?php $q=$questions[39];?>
+	    	answer.innerHTML="dans else";
 		}
     	question.innerHTML="<?php echo $q[1];?>";
-    	answer.innerHTML="compteur :"+compteur;
+    	//answer.innerHTML="compteur :"+compteur;
     	compteur++;
 
     }
