5cbb7dacb58b44cef29cf6ad918302051ea4f006
EmmaMansalier <emma.mansalier@reseau.eseo.fr>
2018-11-22 15:02:20 +0100

    m

--- design4green/pageweb.php
+++ design4green/pageweb.php
@@ -24,7 +24,7 @@
 <p onclick="myFunction()"></p>
 
 <form>
- <div id="questionultiple">
+ <div id="questionmultiple">
  	 <fieldset>
  		 <div id="question">success</div>
     		  <p class="flip" onclick="myFunction()">Question suivante</p>
