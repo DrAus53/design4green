12774b40735f541a2a60cb1578a92f83fba7941e
Tanguy SUARD <suard.tanguy@gmail.com>
2018-11-22 10:26:43 +0100

    retour en errière

--- design4green/dbreadquestion.php
+++ design4green/dbreadquestion.php
@@ -2,29 +2,23 @@
 echo "<table style='border: solid 1px black;'>";
 echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
 
-class TableRows extends RecursiveIteratorIterator
-{
-
-    function __construct($it)
-    {
-        parent::__construct($it, self::LEAVES_ONLY);
+class TableRows extends RecursiveIteratorIterator { 
+    function __construct($it) { 
+        parent::__construct($it, self::LEAVES_ONLY); 
     }
 
-    function current()
-    {
-        return "<td style='width:150px;border:1px solid black;'>" . parent::current() . "</td>";
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
     }
 
-    function beginChildren()
-    {
-        echo "<tr>";
-    }
+    function beginChildren() { 
+        echo "<tr>"; 
+    } 
 
-    function endChildren()
-    {
+    function endChildren() { 
         echo "</tr>" . "\n";
-    }
-}
+    } 
+} 
 
 $servername = "localhost";
 $username = "damien";
@@ -34,26 +28,16 @@ $dbname = "brocosurvey";
 try {
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmtQuestion = $conn->prepare("SELECT id, type_reponse, valeur FROM Question");
-    $stmtQuestion->execute();
+    $stmt = $conn->prepare("SELECT id, type_reponse, valeur FROM Question"); 
+    $stmt->execute();
 
-    $questions = array();
     // set the resulting array to associative
-    $result = $stmtQuestion->setFetchMode(PDO::FETCH_ASSOC);
-    while ($donnesQuestion = $stmtQuestion->fetch()) {
-        $idQ = $donnesQuestion['id'];
-        $stmtReponse = $conn->prepare("SELECT id, id_quest_suiv, valeur, champ_select, champ_texte FROM Response WHERE id_question=" + $idQ);
-        $stmtReponse->execute();
-        $reponses = [];
-        
-        $result = $stmtReponse->setFetchMode(PDO::FETCH_ASSOC);
-        while ($donnesReponse = $stmtReponse->fetch()) {
-            //$reponses[] = array($donnesReponse['id'], $donnesReponse['id_quest_suiv'], $donnesReponse['valeur'], $donnesReponse['champ_select'], $donnesReponse['champ_texte']);
-        }
-        $questions[$idQ] = array($donnesQuestion['type_reponse'], $donnesQuestion['valeur'],  $reponses);
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) { 
+        echo $v;
     }
-    echo $questions;
-} catch (PDOException $e) {
+}
+catch(PDOException $e) {
     echo "Error: " . $e->getMessage();
 }
 $conn = null;
