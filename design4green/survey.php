18bb1b1b1741064336dc124f5d62a9524a07acba
Tanguy SUARD <suard.tanguy@gmail.com>
2018-11-23 09:01:43 +0100

    questions

--- design4green/survey.php
+++ design4green/survey.php
@@ -50,42 +50,18 @@ include ('dbread.php');
     	var question = document.getElementById("surveyQuestion");
     	var answer = document.getElementById("surveyAnswer");
     	
-		if (compteur==1) {
-			<?php $q=$questions[1];?>
-	    	question.innerHTML="<?php echo $q[1];?>";
-	    	<?php $r=$q[2];?>
+			<?php foreach ($q as $quest){?>
+	    	question.innerHTML="<?php echo $quest[1];?>";
+	    	<?php $r=$quest[2];?>
 	    	answer.innerHTML="<?php
 	    	foreach ($r as $reponse) {
 	    	    echo $reponse[2];
 	    	    echo "<br>";
 	    	}
+			}
 	    	?>";
 	    	
-		} else if (compteur==2) {
-			<?php $q=$questions[2];?>
-			question.innerHTML="<?php echo $q[1];?>";
-            <?php $r=$q[2];?>
-	    	answer.innerHTML="<?php echo $r[3][2];?>";
-	    	
-		} else if (compteur==3) {
-			<?php $q=$questions[3];?>
-			question.innerHTML="<?php echo $q[1];?>";
-			
-		} else if (compteur==4) {
-			<?php $q=$questions[4];?>
-			question.innerHTML="<?php echo $q[1];?>";
-			
-		} else if (compteur==5) {
-			<?php $q=$questions[5];?>
-			question.innerHTML="<?php echo $q[1];?>";
-			
-		} else if (compteur==6) {
-			<?php $q=$questions[6];?>
-			question.innerHTML="<?php echo $q[1];?>";
-			
-		} else {
-	    	question.innerHTML="No more questions.";
-		}
+
 
     	compteur++;
     }
