6006fcaf41fc5cea7aab05a09352546a0a8a5eb2
Damien Maymard <Damien Maymard@172.16.112.39>
2018-11-23 10:28:35 +0100

    0

--- design4green/surveySauv.php
+++ design4green/surveySauv.php
@@ -95,75 +95,489 @@ include ('dbread.php');
     	var question = document.getElementById("surveyQuestion");
     	var answer = document.getElementById("surveyAnswer");
     	
-		if (compteur==1) {
-			<?php $q=$questions[1];?>
-	    	question.innerHTML="<?php echo $q[1];?>";
-	    	<?php $r=$q[2];?>
-	    	answer.innerHTML="<?php
-    foreach ($r as $reponse) {
-        echo $reponse[2];
-        echo "<br>";
-    }
-    ?>";
-	    	
-		} else if (compteur==2) {
-			<?php $q=$questions[2];?>
-			question.innerHTML="<?php echo $q[1];?>";
-	    	<?php $r=$q[2];?>
-	    	answer.innerHTML="<?php
-    foreach ($r as $reponse) {
-        echo $reponse[2];
-        echo "<br>";
-    }
-    ?>";
-	    	
-		} else if (compteur==3) {
-			<?php $q=$questions[3];?>
-			question.innerHTML="<?php echo $q[1];?>";
-	    	<?php $r=$q[2];?>
-	    	answer.innerHTML="<?php
-    foreach ($r as $reponse) {
-        echo $reponse[2];
-        echo "<br>";
-    }
-    ?>";
-			
-		} else if (compteur==4) {
-			<?php $q=$questions[4];?>
-			question.innerHTML="<?php echo $q[1];?>";
-	    	<?php $r=$q[2];?>
-	    	answer.innerHTML="<?php
-    foreach ($r as $reponse) {
-        echo $reponse[2];
-        echo "<br>";
-    }
-    ?>";
-			
-		} else if (compteur==5) {
-			<?php $q=$questions[5];?>
-			question.innerHTML="<?php echo $q[1];?>";
-	    	<?php $r=$q[2];?>
-	    	answer.innerHTML="<?php
-    foreach ($r as $reponse) {
-        echo $reponse[2];
-        echo "<br>";
-    }
-    ?>";
-			
-		} else if (compteur==6) {
-			<?php $q=$questions[6];?>
-			question.innerHTML="<?php echo $q[1];?>";
-	    	<?php $r=$q[2];?>
-	    	answer.innerHTML="<?php
-    foreach ($r as $reponse) {
-        echo $reponse[2];
-        echo "<br>";
-    }
-    ?>";
+        if (compteur == 1) {
+            <?php $q = $questions[1];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 2) {
+            <?php $q = $questions[2];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 3) {
+            <?php $q = $questions[3];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 4) {
+            <?php $q = $questions[4];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 5) {
+            <?php $q = $questions[5];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 6) {
+            <?php $q = $questions[6];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 11) {
+            <?php $q = $questions[11];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 12) {
+            <?php $q = $questions[12];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 13) {
+            <?php $q = $questions[13];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 14) {
+            <?php $q = $questions[14];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 15) {
+            <?php $q = $questions[15];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 16) {
+            <?php $q = $questions[16];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 17) {
+            <?php $q = $questions[17];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+        } else if (compteur == 18) {
+            <?php $q = $questions[18];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+        } else if (compteur == 6) {
+            <?php $q = $questions[31];?>
+            question.innerHTML = "<?php echo $q[31];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 39) {
+            <?php $q = $questions[39];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 40) {
+            <?php $q = $questions[40];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 41) {
+            <?php $q = $questions[41];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 42) {
+            <?php $q = $questions[42];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 43) {
+            <?php $q = $questions[43];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 44) {
+            <?php $q = $questions[44];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 45) {
+            <?php $q = $questions[45];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 46) {
+            <?php $q = $questions[46];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 47) {
+            <?php $q = $questions[47];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 48) {
+            <?php $q = $questions[48];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 49) {
+            <?php $q = $questions[49];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 50) {
+            <?php $q = $questions[50];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 62) {
+            <?php $q = $questions[62];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 63) {
+            <?php $q = $questions[63];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 70) {
+            <?php $q = $questions[70];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 71) {
+            <?php $q = $questions[71];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 72) {
+            <?php $q = $questions[72];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 73) {
+            <?php $q = $questions[73];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+        } else if (compteur == 74) {
+            <?php $q = $questions[74];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 75) {
+            <?php $q = $questions[75];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 76) {
+            <?php $q = $questions[76];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 77) {
+            <?php $q = $questions[77];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 78) {
+            <?php $q = $questions[78];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 83) {
+            <?php $q = $questions[83];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 84) {
+            <?php $q = $questions[84];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 85) {
+            <?php $q = $questions[85];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 86) {
+            <?php $q = $questions[86];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 87) {
+            <?php $q = $questions[87];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
+
+        } else if (compteur == 88) {
+            <?php $q = $questions[88];?>
+            question.innerHTML = "<?php echo $q[1];?>";
+            <?php $r = $q[2];?>
+            answer.innerHTML = "<?php
+                foreach ($r as $reponse) {
+                    echo $reponse[2];
+                    echo "<br>";
+                }
+                ?>";
 			
 		} else {
-	    	question.innerHTML="No more questions.";
-	    	answer.innerHTML="Then no more answers too.";
+	    	question.innerHTML="Press next button again.";
 		}
 
     	compteur++;
